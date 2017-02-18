d8691770be964ea31ae13b0094f1fc97a28a6118
Idan Hazan <as4ny0o@gmail.com>
2017-02-19 00:09:34 +0200

    Update Harass.cs

--- SmartCast Udyr/Modes/Harass.cs
+++ SmartCast Udyr/Modes/Harass.cs
@@ -1,8 +1,10 @@
-﻿using EloBuddy;
+using EloBuddy;
 using EloBuddy.SDK;
+using EloBuddy.SDK.Menu.Values;
 using System.Collections.Generic;
 using System.Linq;
 using static SmartCast.Abilities;
+using static SmartCast.Settings;
 using static SmartCast.SummonerSpells;
 using static SmartCast.Utilities;
 
@@ -19,15 +21,19 @@ namespace SmartCast.Modes
 
             if (targets == null || targets.Count == 0)
                 return;
-            else if (KillSteal())
+
+            int Range = Fight["Enemies.Range"].Cast<Slider>().CurrentValue;
+            int Health = Fight["Enemies.Health"].Cast<Slider>().CurrentValue;
+
+            if (KillSteal())
                 return;
-            else if (Stun())
+            else if (Stun(Range))
                 return;
-            else if (Shield())
+            else if (Shield(Health))
                 return;
-            else if (Attack())
+            else if (Attack(Range))
                 return;
-            else if (Basic())
+            else if (Basic(Range))
                 return;
         }
 
@@ -51,12 +57,12 @@ namespace SmartCast.Modes
             return false;
         }
 
-        private static bool Stun()
+        private static bool Stun(int Range)
         {
             if (!E.IsReady() && !Udyr.HasBuff(Buffs["E.Stance"]))
                 return false;
 
-            AIHeroClient target = targets.OrderBy(Enemy => Enemy.Distance(Udyr)).Where(Enemy => IsValid(Enemy, 600) && !Enemy.HasBuffOfType(BuffType.Stun) && Stunable(Enemy)).FirstOrDefault();
+            AIHeroClient target = targets.OrderBy(Enemy => Enemy.Distance(Udyr)).Where(Enemy => IsValid(Enemy, Range) && !Enemy.HasBuffOfType(BuffType.Stun) && Stunable(Enemy)).FirstOrDefault();
 
             if (target == null)
                 return false;
@@ -67,7 +73,7 @@ namespace SmartCast.Modes
             return true;
         }
 
-        private static bool Shield()
+        private static bool Shield(int Health)
         {
             if (!W.IsReady() || TigerEffect() && PhoenixEffect())
                 return false;
@@ -78,7 +84,7 @@ namespace SmartCast.Modes
 
             bool CastQ = TigerUdyr && Q.IsReady();
             bool CastR = PhoenixUdyr && R.IsReady();
-            bool CastQorR = HybridUdyr && (Q.IsReady() || R.IsReady()) && Udyr.HealthPercent > 65;
+            bool CastQorR = HybridUdyr && (Q.IsReady() || R.IsReady()) && Udyr.HealthPercent > Health;
 
             if (CastQ || CastR || CastQorR)
                 return false;
@@ -87,7 +93,7 @@ namespace SmartCast.Modes
             return true;
         }
 
-        private static bool Attack()
+        private static bool Attack(int Range)
         {
             if (!Q.IsReady() && !R.IsReady())
                 return false;
@@ -96,7 +102,7 @@ namespace SmartCast.Modes
             bool PhoenixUdyr = W.Level <= R.Level && Q.Level < W.Level;
             bool HybridUdyr = W.Level <= Q.Level && W.Level <= R.Level;
 
-            AIHeroClient target = targets.OrderBy(Enemy => Enemy.Health).Where(Enemy => IsValid(Enemy, 600) && !Enemy.HasUndyingBuff()).FirstOrDefault();
+            AIHeroClient target = targets.OrderBy(Enemy => Enemy.Health).Where(Enemy => IsValid(Enemy, Range) && !Enemy.HasUndyingBuff()).FirstOrDefault();
 
             if (target == null)
                 return false;
@@ -109,9 +115,9 @@ namespace SmartCast.Modes
             return true;
         }
 
-        private static bool Basic()
+        private static bool Basic(int Range)
         {
-            AIHeroClient target = targets.OrderBy(Enemy => Enemy.Health).Where(Enemy => IsValid(Enemy, 600) && !Enemy.HasUndyingBuff()).FirstOrDefault();
+            AIHeroClient target = targets.OrderBy(Enemy => Enemy.Health).Where(Enemy => IsValid(Enemy, Range) && !Enemy.HasUndyingBuff()).FirstOrDefault();
 
             if (target == null)
                 return false;
