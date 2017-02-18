062550c1bbbe10176a1c9bd645c7347a81ee36f1
Idan Hazan <as4ny0o@gmail.com>
2017-02-19 00:11:07 +0200

    Update Mode.cs

--- SmartCast Udyr/Modes/Mode.cs
+++ SmartCast Udyr/Modes/Mode.cs
@@ -1,9 +1,11 @@
-﻿using EloBuddy.SDK;
 using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu.Values;
 using System.Collections.Generic;
 using System.Linq;
 using static SmartCast.Abilities;
 using static SmartCast.Items;
+using static SmartCast.Settings;
 using static SmartCast.Utilities;
 
 namespace SmartCast.Modes
@@ -19,11 +21,14 @@ namespace SmartCast.Modes
 
         private static void CriticalShield(int Enemies, int Minions, int Monsters)
         {
-            if (!W.IsReady() || Udyr.HealthPercent > 30)
+            int HealthVsEnemies = Danger["Enemies.Health"].Cast<Slider>().CurrentValue;
+            int HealthVsMinionsAndMonsters = Danger["MinionsAndMonsters.Health"].Cast<Slider>().CurrentValue;
+
+            if (!W.IsReady())
                 return;
-            else if (Udyr.HealthPercent <= 30 && Enemies > 0)
+            else if (Udyr.HealthPercent <= HealthVsEnemies && Enemies > 0)
                 W.Cast();
-            else if (Udyr.Health <= 150 && (Minions > 0 || Monsters > 0))
+            else if (Udyr.Health <= HealthVsMinionsAndMonsters && (Minions > 0 || Monsters > 0))
                 W.Cast();
         }
 
@@ -42,20 +47,33 @@ namespace SmartCast.Modes
 
         internal static dynamic GetTarget(Target target)
         {
+            int RangeVsEnemies = Danger["Enemies.Range"].Cast<Slider>().CurrentValue;
+            int RangeVsMinionsAndMonsters = Danger["MinionsAndMonsters.Range"].Cast<Slider>().CurrentValue;
+
             List<AIHeroClient> Enemies = new List<AIHeroClient>();
-            Enemies = EntityManager.Heroes.Enemies.Where(Enemy => Enemy.IsInRange(Udyr, 600)).ToList();
+            Enemies = EntityManager.Heroes.Enemies.Where(Enemy => Enemy.IsInRange(Udyr, RangeVsEnemies)).ToList();
 
             List<Obj_AI_Minion> Minions = new List<Obj_AI_Minion>();
-            Minions = EntityManager.MinionsAndMonsters.EnemyMinions.Where(Minion => Minion.IsInRange(Udyr, 300)).ToList();
+            Minions = EntityManager.MinionsAndMonsters.EnemyMinions.Where(Minion => Minion.IsInRange(Udyr, RangeVsMinionsAndMonsters)).ToList();
 
             List<Obj_AI_Minion> Monsters = new List<Obj_AI_Minion>();
-            Monsters = EntityManager.MinionsAndMonsters.Monsters.Where(Monster => Monster.IsInRange(Udyr, 300)).ToList();
+            Monsters = EntityManager.MinionsAndMonsters.Monsters.Where(Monster => Monster.IsInRange(Udyr, RangeVsMinionsAndMonsters)).ToList();
 
             if (Enemies.Count != 0 || Minions.Count != 0 || Monsters.Count != 0)
             {
                 CriticalShield(Enemies.Count, Minions.Count, Monsters.Count);
                 UsePotion();
             }
+
+            int RangeEnemies = Fight["Enemies.Range"].Cast<Slider>().CurrentValue;
+            int RangeMinionsAndMonsters = Fight["MinionsAndMonsters.Range"].Cast<Slider>().CurrentValue;
+
+            if (target == Target.Enemy && RangeVsEnemies != RangeEnemies)
+                Enemies = EntityManager.Heroes.Enemies.Where(Enemy => Enemy.IsInRange(Udyr, RangeEnemies)).ToList();
+            else if (target == Target.Minion && RangeVsMinionsAndMonsters != RangeMinionsAndMonsters)
+                Minions = EntityManager.MinionsAndMonsters.EnemyMinions.Where(Minion => Minion.IsInRange(Udyr, RangeMinionsAndMonsters)).ToList();
+            else if (target == Target.Monster && RangeVsMinionsAndMonsters != RangeMinionsAndMonsters)
+                Monsters = EntityManager.MinionsAndMonsters.Monsters.Where(Monster => Monster.IsInRange(Udyr, RangeMinionsAndMonsters)).ToList();
 
             if (target == Target.Enemy)
                 return Enemies;
