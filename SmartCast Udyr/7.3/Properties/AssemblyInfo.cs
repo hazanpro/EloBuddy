3d81bf0b010b22387c4d3bcbbac98f3e681b49df
Idan Hazan <as4ny0o@gmail.com>
2017-03-02 22:03:43 +0200

    Update AssemblyInfo.cs

--- SmartCast Udyr/7.3/Properties/AssemblyInfo.cs
+++ SmartCast Udyr/7.3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SmartCast Udyr")]
+[assembly: AssemblyTitle("SmartCast Udyr 7.3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SmartCast Udyr 7.3")]
+[assembly: AssemblyProduct("SmartCast Udyr")]
 [assembly: AssemblyCopyright("Copyright © hazanpro 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
